69bed9fed17fd74346a056749688b20b6fb24beb
W47 KRTS <postuionut47@gmail.com>
2024-09-05 11:36:48 +0000

    app

--- backend-flask/app.py
+++ backend-flask/app.py
@@ -30,7 +30,7 @@ from aws_xray_sdk.ext.flask.middleware import XRayMiddleware
 
 xray_url = os.getenv("AWS_XRAY_URL")
 xray_recorder.configure(service='backend-flask', dynamic_naming=xray_url)
-XRayMiddleware(app, xray_recorder)
+
  
 
 # HoneyComb ----------
@@ -44,6 +44,9 @@ tracer = trace.get_tracer(__name__)
 
 
 app = Flask(__name__)
+
+# X-RAY ---------------
+XRayMiddleware(app, xray_recorder)
 
 # HoneyComb ----------
 # Initialize automatic intrumentation with Flask
