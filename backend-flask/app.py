e138d190158bfe6c766f5cd57716629114063486
W47 KRTS <postuionut47@gmail.com>
2024-09-05 07:28:41 +0000

    ggg

--- backend-flask/app.py
+++ backend-flask/app.py
@@ -21,7 +21,17 @@ from opentelemetry.instrumentation.requests import RequestsInstrumentor
 from opentelemetry.exporter.otlp.proto.http.trace_exporter import OTLPSpanExporter
 from opentelemetry.sdk.trace import TracerProvider
 from opentelemetry.sdk.trace.export import BatchSpanProcessor
+from opentelemetry.sdk.trace.export import ConsoleSpanExporter, SimpleSpanProcessor
 
+
+# X-RAY ---------------
+from aws_xray_sdk.core import xray_recorder
+from aws_xray_sdk.ext.flask.middleware import XRayMiddleware
+
+xray_url = os.getenv("AWS_XRAY_URL")
+xray_recorder.configure(service='backend-flask', dynamic_naming=xray_url)
+XRayMiddleware(app, xray_recorder)
+ 
 
 # HoneyComb ----------
 # Initialize tracing and an exporter that can send data to Honeycomb
