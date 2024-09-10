5666f3b6ae38884ad41ccec7249cd6b4fc81cb76
W47 KRTS <postuionut47@gmail.com>
2024-09-10 10:05:47 +0000

    wt

--- backend-flask/app.py
+++ backend-flask/app.py
@@ -28,6 +28,26 @@ from opentelemetry.sdk.trace.export import ConsoleSpanExporter, SimpleSpanProces
 from aws_xray_sdk.core import xray_recorder
 from aws_xray_sdk.ext.flask.middleware import XRayMiddleware
 
+# WATCHTOWER----------- 
+import watchtower
+import logging
+from time import strftime
+
+
+
+# Configuring Logger to Use CloudWatch
+LOGGER = logging.getLogger(__name__)
+LOGGER.setLevel(logging.DEBUG)
+console_handler = logging.StreamHandler()
+cw_handler = watchtower.CloudWatchLogHandler(log_group='cruddur')
+LOGGER.addHandler(console_handler)
+LOGGER.addHandler(cw_handler)
+LOGGER.info("some message")
+
+
+
+
+
 xray_url = os.getenv("AWS_XRAY_URL")
 xray_recorder.configure(service='backend-flask', dynamic_naming=xray_url)
 
@@ -44,6 +64,13 @@ tracer = trace.get_tracer(__name__)
 
 
 app = Flask(__name__)
+
+# WATCHTOWER----------- 
+@app.after_request
+def after_request(response):
+    timestamp = strftime('[%Y-%b-%d %H:%M]')
+    LOGGER.error('%s %s %s %s %s %s', timestamp, request.remote_addr, request.method, request.scheme, request.full_path, response.status)
+    return response
 
 # X-RAY ---------------
 XRayMiddleware(app, xray_recorder)
