33f018e1b5f43e4aaa33f5aebcd721b130da2a27
W47 KRTS <postuionut47@gmail.com>
2024-09-17 11:51:43 +0000

    conf page

--- frontend-react-js/src/pages/SignupPage.js
+++ frontend-react-js/src/pages/SignupPage.js
@@ -3,8 +3,7 @@ import React from "react";
 import {ReactComponent as Logo} from '../components/svg/logo.svg';
 import { Link } from "react-router-dom";
 import { Auth } from 'aws-amplify';
-// [TODO] Authenication
-import Cookies from 'js-cookie'
+
 
 
 export default function SignupPage() {
@@ -15,29 +14,28 @@ export default function SignupPage() {
   const [username, setUsername] = React.useState('');
   const [password, setPassword] = React.useState('');
   const [errors, setErrors] = React.useState('');
-  const [cognitoErrors, setCognitoErrors] = React.useState('');
 
   const onsubmit = async (event) => {
     event.preventDefault();
-    setCognitoErrors('')
+    setErrors('')
     try {
-        const { user } = await Auth.signUp({
-          username: email,
-          password: password,
-          attributes: {
-              name: name,
-              email: email,
-              preferred_username: username,
-          },
-          autoSignIn: { // optional - enables auto sign in after user is confirmed
-              enabled: true,
-          }
-        });
-        console.log(user);
-        window.location.href = `/confirm?email=${email}`
+      const { user } = await Auth.signUp({
+        username: email,
+        password: password,
+        attributes: {
+          name: name,
+          email: email,
+          preferred_username: username,
+        },
+        autoSignIn: { // optional - enables auto sign in after user is confirmed
+          enabled: true,
+        }
+      });
+      console.log(user);
+      window.location.href = `/confirm?email=${email}`
     } catch (error) {
         console.log(error);
-        setCognitoErrors(error.message)
+        setErrors(error.message)
     }
     return false
   }
