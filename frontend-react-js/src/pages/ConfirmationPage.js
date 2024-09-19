e97628f79187616207a95a92028c87fe66f7b6b8
W47 KRTS <postuionut47@gmail.com>
2024-09-19 06:38:16 +0000

    cf

--- frontend-react-js/src/pages/ConfirmationPage.js
+++ frontend-react-js/src/pages/ConfirmationPage.js
@@ -36,7 +36,7 @@ export default function ConfirmationPage() {
       if (err.message == 'Username cannot be empty'){
         setErrors("You need to provide an email in order to send Resend Activiation Code")   
       } else if (err.message == "Username/client id combination not found."){
-        setCognitoErrors("Email is invalid or cannot be found.")   
+        setErrors("Email is invalid or cannot be found.")   
       }
     }
   }
