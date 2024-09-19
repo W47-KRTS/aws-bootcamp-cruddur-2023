8f7e9cb3675f4ed9f757158b83e106fd64ed2e56
W47 KRTS <postuionut47@gmail.com>
2024-09-19 06:55:13 +0000

    hm

--- frontend-react-js/src/pages/HomeFeedPage.js
+++ frontend-react-js/src/pages/HomeFeedPage.js
@@ -2,7 +2,7 @@ import './HomeFeedPage.css';
 import React from "react";
 // import './configureAmplify';
 
-import { Auth, Hub, signIn, signOut} from 'aws-amplify';
+import {Auth} from 'aws-amplify';
 
 
 import DesktopNavigation  from '../components/DesktopNavigation';
@@ -26,6 +26,9 @@ export default function HomeFeedPage() {
     try {
       const backend_url = `${process.env.REACT_APP_BACKEND_URL}/api/activities/home`
       const res = await fetch(backend_url, {
+        headers: {
+          Authorization: `Bearer ${localStorage.getItem("access_token")}`
+        },
         method: "GET"
       });
       let resJson = await res.json();
