1efd1a00b05ddfecff055c13705c18990bd099ee
W47 KRTS <postuionut47@gmail.com>
2024-09-27 09:48:02 +0000

    flkaws

--- backend-flask/lib/cognito_token_verification.py
+++
@@ -1,6 +0,0 @@
-import time 
-import requests
-from jose import jwkm jwt
-from jose.exceptions import JOSEError
-from jose.utils import base64url_decode
-from flask_awscognito.exceptions import FlaskAwsCognitoerror, TokenverifyError
