82d935f7e580af249227a4a04cb8075174dbda8e
W47 KRTS <postuionut47@gmail.com>
2024-10-29 11:07:59 +0000

    post-confirm

--- aws/lamdas/cruddur-post-confirmation.py
+++ aws/lamdas/cruddur-post-confirmation.py
@@ -4,12 +4,29 @@ import psycopg2
 
 def lamda_handler(event, context):
     user = event['request']['userAttributes']
+    user_display_name = user['name']
+    user_email = user['email']
+    user_cognito_id = user['sub']
+
+
     try:
         conn = psycopg2.connect(os.getenv('CONNECTION_URL'))
         cur = conn.cursor()
+        (user['name'], user['email'], user['sub'])
+
+        
+
         sql = f"""
-        "INSERT INTO users (display_name, handle, cognito_user_id) 
-        VALUES({name}, {email}, {sub})"
+        "INSERT INTO users (
+        display_name, 
+        handle, 
+        cognito_user_id
+        ) 
+        VALUES(
+            {user_display_name}, 
+            {user_email}, 
+            {user_cognito_id}
+        )"
         """
         cur.execute("INSERT INTO users (display_name, handle, cognito_user_id) VALUES(%s, %s, %s)", (user['name'], user['email'], user['sub']))
         conn.commit()
