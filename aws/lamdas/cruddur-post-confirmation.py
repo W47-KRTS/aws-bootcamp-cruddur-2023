cfb6c922a7ee72390a8b0e9f8650eb2db1b081af
W47 KRTS <postuionut47@gmail.com>
2024-10-29 11:15:02 +0000

    post_confirm

--- aws/lamdas/cruddur-post-confirmation.py
+++ aws/lamdas/cruddur-post-confirmation.py
@@ -4,31 +4,34 @@ import psycopg2
 
 def lamda_handler(event, context):
     user = event['request']['userAttributes']
-    user_display_name = user['name']
-    user_email = user['email']
-    user_cognito_id = user['sub']
+    print('userAttributes')
+    print(user)
 
+    user_display_name   = user['name']
+    user_email          = user['email']
+    user_handle          = user['preffered_username']
+    user_cognito_id     = user['sub']
 
     try:
         conn = psycopg2.connect(os.getenv('CONNECTION_URL'))
         cur = conn.cursor()
         (user['name'], user['email'], user['sub'])
 
-        
-
         sql = f"""
         "INSERT INTO users (
         display_name, 
+        email,
         handle, 
         cognito_user_id
         ) 
         VALUES(
             {user_display_name}, 
             {user_email}, 
+            {user_handle}
             {user_cognito_id}
         )"
         """
-        cur.execute("INSERT INTO users (display_name, handle, cognito_user_id) VALUES(%s, %s, %s)", (user['name'], user['email'], user['sub']))
+        cur.execute(sql)
         conn.commit()
 
     except(Exception, psycopg2.DatabaseError) as error:
