576894f09dc11e982fa357b87c4a4593714578c0
W47 KRTS <postuionut47@gmail.com>
2025-01-02 11:18:23 +0000

    hm_actv update

--- backend-flask/services/home_activities.py
+++ backend-flask/services/home_activities.py
@@ -2,39 +2,44 @@ from datetime import datetime, timedelta, timezone
 # from opentelemetry import trace
 import logging
 
-from lib.db import pool, query_wrap_array
+from lib.db import db
 
+#from lib.db import pool, query_wrap_array
+#pool = lib.db.pool  
+#query_wrap_array = lib.query_wrap_array
 # tracer = trace.get_tracer("home.activities")
 
 class HomeActivities:
   def run(cognito_user_id=None):
+    sql = db.template('activities', 'home')
+    results = db.query_array_json(sql)
     #LOGGER.info("HomeActivities")
     # with tracer.start_as_current_span("home-activities-mock-data"):
     #  span = trace.get_current_span()
     #  now = datetime.now(timezone.utc).astimezone()
     #  span.set_attribute("app.now", now.isoformat())
 
-      sql = query_wrap_array("""SELECT
-        activities.uuid,
-        users.display_name,
-        users.handle,
-        activities.message,
-        activities.replies_count,
-        activities.reposts_count,
-        activities.likes_count,
-        activities.reply_to_activity_uuid,
-        activities.expires_at,
-        activities.created_at
-      FROM public.activities
-      LEFT JOIN public.users ON users.uuid = activities.user_uuid
-      ORDER BY activities.created_at DESC
-      """)
-      print(sql)
-      with pool.connection() as conn:
-        with conn.cursor() as cur:
-          cur.execute(sql)
-          # this will return a tuple
-          # the first field being the data
-          json = cur.fetchone()
-      return json[0]
-      return results
+      #sql = query_wrap_array("""SELECT
+      #  activities.uuid,
+      #  users.display_name,
+      #  users.handle,
+      #  activities.message,
+      #  activities.replies_count,
+      #  activities.reposts_count,
+      #  activities.likes_count,
+      #  activities.reply_to_activity_uuid,
+      #  activities.expires_at,
+      #  activities.created_at
+      #FROM public.activities
+      #LEFT JOIN public.users ON users.uuid = activities.user_uuid
+      #ORDER BY activities.created_at DESC
+      #""")
+      #print(sql)
+      #with pool.connection() as conn:
+      #  with conn.cursor() as cur:
+      #    cur.execute(sql)
+      #    # this will return a tuple
+      #    # the first field being the data
+      #    json = cur.fetchone()
+      #return json[0]
+    return results
