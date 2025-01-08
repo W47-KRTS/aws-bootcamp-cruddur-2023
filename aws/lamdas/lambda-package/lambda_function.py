614582b432d6f7d5bf0a3173bdd3f0610805b0c8
W47 KRTS <postuionut47@gmail.com>
2025-01-08 17:53:19 +0000

    lambda-package

---
+++ aws/lamdas/lambda-package/lambda_function.py
@@ -0,0 +1,50 @@
+import json
+import psycopg2
+import os
+
+def lambda_handler(event, context):
+    user = event['request']['userAttributes']
+    print('userAttributes')
+    print(user)
+
+    user_display_name  = user['name']
+    user_email         = user['email']
+    user_handle        = user['preferred_username']
+    user_cognito_id    = user['sub']
+    conn = None
+    try:
+        print('entered-try')
+        sql = """
+            INSERT INTO public.users (
+                display_name, 
+                email,
+                handle, 
+                cognito_user_id
+            ) 
+            VALUES (%s, %s, %s, %s)
+        """
+        print('SQL Statement ----')
+        print(sql)
+        conn = psycopg2.connect(os.getenv('CONNECTION_URL'))
+        cur = conn.cursor()
+
+        params = [
+            user_display_name,
+            user_email,
+            user_handle,
+            user_cognito_id
+        ]
+        cur.execute(sql, params)
+        conn.commit() 
+        print("Data inserted successfully")
+    
+    except Exception as error:
+        print(f"An error occurred: {error}")
+    
+    finally:
+        if conn is not None:
+            cur.close()
+            conn.close()
+            print('Database connection closed.')
+
+    return event
---
+++ aws/lamdas/readme.txt
@@ -0,0 +1,3 @@
+The lambda-package contains the zip file to be uploaded in the AWS for the 
+lambda code. It contains the correct lambda code and the psycopg2-binary
+and the manylinux2014 package. 
